bfcc6ad1984367b812862bf2a7caa71f53acaeb9
charlespctech <charles.pctech@gmail.com>
2016-10-30 15:01:40 -0500

    wow

--- Chapter 3 Variables And Assignment/ch3.rb
+++ Chapter 3 Variables And Assignment/ch3.rb
@@ -1,5 +1,3 @@
-myString = '...you can say that again...'
-puts myString
-puts myString
-
-test2
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow!  ' + name + ' is a really long name!'
